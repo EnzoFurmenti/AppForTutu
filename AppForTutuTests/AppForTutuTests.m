35e1e5d772fc598f6ef16e742a419e769a336f53
EnzoF <EnzoF@macbook-pro-enzof.local>
2016-02-18 14:42:50 +0300

    Initial Commit

---
+++ AppForTutuTests/AppForTutuTests.m
@@ -0,0 +1,39 @@
+//
+//  AppForTutuTests.m
+//  AppForTutuTests
+//
+//  Created by EnzoF on 18.02.16.
+//  Copyright © 2016 EnzoF. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface AppForTutuTests : XCTestCase
+
+@end
+
+@implementation AppForTutuTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
